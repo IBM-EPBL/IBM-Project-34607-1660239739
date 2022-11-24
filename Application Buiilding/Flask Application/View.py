544d1e19d27c9a0f15ca7b09252dab5a4a30f1cc
ASWITHA-K-G <110890733+ASWITHA-K-G@users.noreply.github.com>
2022-11-25 00:14:58 +0530

    Update View.py

--- Application Buiilding/Flask Application/View.py
+++ Application Buiilding/Flask Application/View.py
@@ -1,67 +1,30 @@
-import cv2 as cv
-from cvzone.HandTrackingModule import HandDetector
+import cv2
 import numpy as np
-import math
-from cvzone.ClassificationModule import Classifier
-import trainlist
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 
-cap=cv.VideoCapture(0)
-detector = HandDetector(maxHands=1)
-offset=20
-img_size=300
-classifier=Classifier("./Model/sign_1.h5","./Model/labels.txt")
-labels=trainlist.dataset
-list=[" "]
-count=0
-
-while True:
-    ret,img=cap.read()
-    img_out=img.copy()
-    hands,img=detector.findHands(img)
-    if hands:
-        hand=hands[0]
-        x,y,w,h=hand['bbox']
-        #Image empty
-        img_bg=np.ones((img_size,img_size,3), np.uint8)*255
-        croped_img=img[y-offset:y+ h+offset,x-offset:x+ w+offset]
-          
-        aspect_ratio=h/w
-       
-        if aspect_ratio>1:
-            k=img_size/h
-            wCal= math.ceil(k*w)
-            img_resize=cv.resize(croped_img,(wCal,img_size))
-            wGap =math.ceil((img_size-wCal)/2)
-            img_bg[:,wGap:wCal+wGap] = img_resize
-            prediction,index=classifier.getPrediction(img_bg)
-            print(labels[index])
-            
-        else:
-            k=img_size/w
-            hCal= math.ceil(k*h)
-            img_resize=cv.resize(croped_img,(img_size,hCal))
-            hGap =math.ceil((img_size-hCal)/2)
-            img_bg[hGap:hCal+hGap,:] = img_resize
-            prediction,index=classifier.getPrediction(img_bg)
-            print(labels[index])
-
-        cv.putText(img_out,labels[index],(x,y-20),cv.FONT_HERSHEY_COMPLEX,2,(255,255,255),2)
-           
-        cv.imshow("Image_croped",croped_img)
-        cv.imshow("Image_bg",img_bg)
-        
-        gesture=labels[index]
-        count+=1
-        if count==30:
-            if gesture!=list[-1]:
-                list.append(gesture)
-            count=count-30
-        print(list)
-    cv.imshow("Image",img_out)
-    key=cv.waitKey(1)
-    
-    if key==ord('q'):
-        break
-
-cap.release()
-cv.destroyAllWindows()
+class Video(object):
+	def __init__(self):
+		self.video = cv2.VideoCapture(0)
+		self.roi_start = (50, 150)
+		self.roi_end = (250, 350)
+		self.model = load_model('aslpng_ibm.h5')
+		self.index=['A','B','C','D','E','F','G','H','I']
+		self.y = None
+	def __del__(self):
+		self.video.release()
+	def get_frame(self):
+		ret,frame = self.video.read()
+		frame = cv2.resize(frame, (640, 480))
+		copy = frame.copy()
+		copy = copy[150:150+200,50:50+200]
+		# Prediction Start
+		cv2.imwrite('image.jpg',copy)
+		copy_img = image.load_img('image.jpg', target_size=(64,64))
+		x = image.img_to_array(copy_img)
+		x = np.expand_dims(x, axis=0)
+		pred = np.argmax(self.model.predict(x), axis=1)
+		self.y = pred[0]
+		cv2.putText(frame,'The Predicted Alphabet is: '+str(self.index[self.y]),(100,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3)
+		ret,jpg = cv2.imencode('.jpg', frame)
+		return jpg.tobytes()
